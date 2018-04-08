1ea0cf7ac3de4f5e3b598079eb52e17c79bf0f71
vindarel <ehvince@mailz.org>
2018-04-08 15:46:07 +0200

    show the magnet link on button click

--- src/torrents-reblocks.lisp
+++ src/torrents-reblocks.lisp
@@ -6,7 +6,8 @@
                 #:defapp)
   (:import-from #:weblocks-ui/form
                 #:with-html-form
-                #:render-form-and-button))
+                #:render-form-and-button)
+  (:export #:start))
 (in-package :torrents-reblocks)
 
 (defapp torrents)
@@ -23,7 +24,7 @@
   (cdr (assoc key alist :test test)))
 
 (defmethod weblocks/session:init ((app torrents))
-  (let (results)
+  (let (results magnet)
     (flet ((query (&key query &allow-other-keys)
              (format t "searching for ~a~&" query)
              (setf results (async-torrents query))
@@ -31,6 +32,7 @@
            (see-magnet (&key index &allow-other-keys)
              (declare (ignorable index))
              (format t "see-magnet~&")
+             (setf magnet (magnet (parse-integer index)))
              (weblocks/widget:update (weblocks/widgets/root:get))))
       (lambda ()
         (with-html
@@ -53,28 +55,34 @@
               (:input :type "submit"
                       :value "Search")))
 
-           (:table :class "ui selectable table"
-            (:thead
-             (:th "Title")
-             (:th "Seeders")
-             (:th "Leechers")
-             (:th ))
-            (:tbody)
-            (dolist (it results)
-              (with-html
-                (:tr
-                 (:td (:a :href (assoc-value it :href)
-                          (assoc-value it :title)))
-                 (:td (assoc-value it :seeders))
-                 (:td (assoc-value it :leechers))
-                 (:td
-                  (with-html-form (:POST #'see-magnet)
-                    (:input :type "hidden"
-                            :name "index"
-                            :value (position it results))
-                    (:input :type "submit"
-                            :class "ui primary button"
-                            :value "see-magnet")))))))))))))
+           (when magnet
+             (:h4 (format nil "magnet link:"))
+             (:div magnet))
+           (when results
+             (:table :class "ui selectable table"
+                     (:thead
+                      (:th "Title")
+                      (:th "Seeders")
+                      (:th "Leechers")
+                      (:th "Source")
+                      (:th ))
+                     (:tbody)
+                     (dolist (it results)
+                       (with-html
+                         (:tr
+                          (:td (:a :href (assoc-value it :href)
+                                   (assoc-value it :title)))
+                          (:td (assoc-value it :seeders))
+                          (:td (assoc-value it :leechers))
+                          (:td (assoc-value it :source))
+                          (:td
+                           (with-html-form (:POST #'see-magnet)
+                             (:input :type "hidden"
+                                     :name "index"
+                                     :value (position it results))
+                             (:input :type "submit"
+                                     :class "ui primary button"
+                                     :value "magnet"))))))))))))))
 
 (defun start ()
   (weblocks/debug:on)
